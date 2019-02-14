130644fef36d8c0b112079b29efb80e3d384874c
Kristóf Mátyás Marsi <mkmdev18@gmail.com>
2019-02-14 13:24:45 +0100

    Added java related wordlist

--- com/hangapp/WordLists.java
+++ com/hangapp/WordLists.java
@@ -3,6 +3,17 @@ package com.hangapp;
 public class WordLists{
   public static String[] wordList() {
     // String[] guesses = {"Andris", "Laci", "Bence"};
-    return new String[]{"Anya", "Apa", "Gyerekek"};
+    return new String[]{"Assign",
+                        "Class",
+                        "Object",
+                        "Constructor",
+                        "Definition",
+                        "Garbage Collection",
+                        "Instantiate",
+                        "Method",
+                        "Primitive",
+                        "Typecast",
+                        "Parameter"
+                      };
   }
 }
